8a49e514443a6e7678194ed4a80aefc676a8f078
Harshada Kadam <harshadakadamjob@gmail.com>
2025-07-26 20:33:57 +0530

    triangle pattern in number with same number in one line in logic building

--- Patterns/numberpatternsamenoinoneline.cpp
+++ Patterns/numberpatternsamenoinoneline.cpp
@@ -7,14 +7,12 @@ int main()
 {
 
     int n = 5;
-    char ch = 'A';
 
     for (int i = 1; i <= n; i++)
     {
         for (int j = 1; j <= i; j++)
         {
-            cout << ch;
-            ch += 1;
+            cout << i <<" ";
         }
         cout << "\n";
     }
