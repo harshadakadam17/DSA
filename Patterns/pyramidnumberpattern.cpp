023ddc3c1ca3f6ce1e1204071af096a14ef426ae
Harshada Kadam <harshadakadamjob@gmail.com>
2025-07-27 15:28:34 +0530

    pyramid pattern in numbers

---
+++ Patterns/pyramidnumberpattern.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n = 4;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n - i - 1; j++)
+        {
+            cout << " ";
+        }
+        for (int k = 1; k <= i + 1; k++)
+        {
+            cout << k;
+        }
+        for (int f = i; f >= 1; f--)
+        {
+            cout << f;
+        }
+            cout << endl;
+    }
+
+
+}
